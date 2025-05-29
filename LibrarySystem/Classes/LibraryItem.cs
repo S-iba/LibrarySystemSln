0c23743a93f1cb3b77e18778cd98ff169a21a8c0
KlaasicMan <klaas.siba@outlook.com>
2025-05-29 11:50:06 +0200

    added libraryItem constructor

--- LibrarySystem/Classes/LibraryItem.cs
+++ LibrarySystem/Classes/LibraryItem.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LibrarySystem.Classes;
+using static System.Console;
+
 
 namespace LibrarySystem.Classes
 {
@@ -16,5 +18,17 @@ namespace LibrarySystem.Classes
         public string Title { get => title; set => title = value; }
         public string ID1 { get => ID; set => ID = value; }
         public int YearPublished { get => yearPublished; set => yearPublished = value; }
+
+        public LibraryItem() {
+            
+            WriteLine("Creating a new library item... \n");
+            WriteLine("Please enter the details below: \n");
+            Write("Enter the title of the item: ");
+            title = ReadLine();
+            Write("\nEnter the ID of the item: ");
+            ID = ReadLine();
+            Write("\nEnter the year the item was published: ");
+            yearPublished = int.Parse(ReadLine());
+        }
     }
 }
