8d86945c216a7349d3bfe4ee60dd9e4ba0f66bfa
S-iba <klaas.siba@outlook.com>
2025-05-30 19:42:57 +0200

    tried a search by ID method

--- LibrarySystem/Collection/LibraryCollection.cs
+++ LibrarySystem/Collection/LibraryCollection.cs
@@ -22,16 +22,27 @@ namespace LibrarySystem.Collection
 
             itemList.Add(item);
         }
+        //removing an Item from the list
         public void RemoveItem(LibraryItem item)
         {
             itemList.Remove(item);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Item has been successfully removed");
         }
-
-        public LibraryItem FindItemByID (string ID)
+        //Search by ID method:
+        public LibraryItem FindById(string ID)
         {
-           return itemList.FirstOrDefault(item => item.ID1 == ID);
-        }
+            foreach (LibraryItem item in itemList) 
+            {
+                if (item.ID1 == ID)
+                {
+                    return item;
+                }
 
+            }
+            return null;
+        }
+        //Display method that uses the object to compensate for thenrest of the details
         public void DisplayItems()
         {
             foreach (var item in itemList)
